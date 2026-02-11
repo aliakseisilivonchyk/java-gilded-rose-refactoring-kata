c85bf9a347475056db30141678577b9afe9a9cc8
Aliaksei Silivonchyk <alexey.silivonchik@gmail.com>
2026-02-11 22:30:19 +0300

    create method for updating item quality to reduce code nesting

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,45 +9,49 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                continue;
-            }
+            updateItemQuality(item);
+        }
+    }
 
-            if (item.name.equals("Aged Brie") || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality < 50) {
-                    item.quality++;
+    private void updateItemQuality(Item item) {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return;
+        }
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality++;
-                            }
+        if (item.name.equals("Aged Brie") || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.quality < 50) {
+                item.quality++;
+
+                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality++;
                         }
+                    }
 
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality++;
-                            }
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality++;
                         }
                     }
                 }
-            } else if (item.quality > 0) {
-                item.quality--;
             }
+        } else if (item.quality > 0) {
+            item.quality--;
+        }
 
-            item.sellIn--;
+        item.sellIn--;
 
-            if (item.sellIn < 0) {
-                if (item.name.equals("Aged Brie")) {
-                    if (item.quality < 50) {
-                        item.quality++;
-                    }
-                } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    item.quality = 0;
-                } else {
-                    if (item.quality > 0) {
-                        item.quality--;
-                    }
+        if (item.sellIn < 0) {
+            if (item.name.equals("Aged Brie")) {
+                if (item.quality < 50) {
+                    item.quality++;
+                }
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                item.quality = 0;
+            } else {
+                if (item.quality > 0) {
+                    item.quality--;
                 }
             }
         }
