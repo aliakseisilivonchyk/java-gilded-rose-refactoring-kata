c7c7eb19f6900da02daf792aeff00de8aa55d120
Aliaksei Silivonchyk <alexey.silivonchik@gmail.com>
2026-02-12 00:07:55 +0300

    add conjured items

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -7,6 +7,7 @@ class GildedRose {
     private static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
     private static final String AGED_BRIE = "Aged Brie";
     private static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String CONJURED = "Conjured";
     private static final int BACKSTAGE_ITEM_SELL_IN_DAY_FIRST_THRESHOLD = 11;
     private static final int BACKSTAGE_ITEM_SELL_IN_DAY_SECOND_THRESHOLD = 6;
 
@@ -40,6 +41,10 @@ class GildedRose {
                     increaseItemQualityIfNotMax(item);
                 }
                 break;
+            case CONJURED:
+                decreaseItemQualityIfNotMin(item);
+                decreaseItemQualityIfNotMin(item);
+                break;
             default:
                 decreaseItemQualityIfNotMin(item);
                 break;
@@ -54,6 +59,10 @@ class GildedRose {
                     break;
                 case BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT:
                     setItemQualityToMin(item);
+                    break;
+                case CONJURED:
+                    decreaseItemQualityIfNotMin(item);
+                    decreaseItemQualityIfNotMin(item);
                     break;
                 default:
                     decreaseItemQualityIfNotMin(item);
